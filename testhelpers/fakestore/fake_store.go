1ebc5abd69e7ae9449e89aa5b38aed37a2d65889
Onsi Fakhouri <pair+onsi@pivotallabs.com>
2013-09-19 17:29:17 -0700

    built out the fake store

---
+++ testhelpers/fakestore/fake_store.go
@@ -0,0 +1,127 @@
+package fakestore
+
+import (
+	"github.com/cloudfoundry/hm9000/models"
+	"github.com/cloudfoundry/hm9000/storeadapter"
+	"time"
+)
+
+type FakeStore struct {
+	ActualIsFresh  bool
+	DesiredIsFresh bool
+
+	ActualFreshnessTimestamp  time.Time
+	DesiredFreshnessTimestamp time.Time
+
+	BumpDesiredFreshnessError error
+	BumpActualFreshnessError  error
+	SaveDesiredStateError     error
+	GetDesiredStateError      error
+	SaveActualStateError      error
+	GetActualStateError       error
+
+	desiredState map[string]models.DesiredAppState
+	actualState  map[string]models.InstanceHeartbeat
+}
+
+func NewFakeStore() *FakeStore {
+	store := &FakeStore{}
+	store.Reset()
+	return store
+}
+
+func (store *FakeStore) Reset() {
+	store.desiredState = make(map[string]models.DesiredAppState, 0)
+	store.actualState = make(map[string]models.InstanceHeartbeat, 0)
+
+	store.ActualIsFresh = false
+	store.DesiredIsFresh = false
+	store.ActualFreshnessTimestamp = time.Time{}
+	store.DesiredFreshnessTimestamp = time.Time{}
+	store.BumpActualFreshnessError = nil
+	store.BumpDesiredFreshnessError = nil
+	store.SaveDesiredStateError = nil
+	store.GetDesiredStateError = nil
+	store.SaveActualStateError = nil
+	store.GetActualStateError = nil
+}
+
+func (store *FakeStore) BumpDesiredFreshness(timestamp time.Time) error {
+	store.DesiredIsFresh = true
+	store.DesiredFreshnessTimestamp = timestamp
+	return store.BumpDesiredFreshnessError
+}
+
+func (store *FakeStore) BumpActualFreshness(timestamp time.Time) error {
+	store.ActualIsFresh = true
+	store.ActualFreshnessTimestamp = timestamp
+	return store.BumpActualFreshnessError
+}
+
+func (store *FakeStore) SaveDesiredState(desiredStates []models.DesiredAppState) error {
+	for _, state := range desiredStates {
+		store.desiredState[state.StoreKey()] = state
+	}
+	return store.SaveDesiredStateError
+}
+
+func (store *FakeStore) GetDesiredState() ([]models.DesiredAppState, error) {
+	if store.GetDesiredStateError != nil {
+		return []models.DesiredAppState{}, store.GetDesiredStateError
+	}
+
+	desireds := make([]models.DesiredAppState, len(store.desiredState))
+
+	i := 0
+	for _, desired := range store.desiredState {
+		desireds[i] = desired
+		i++
+	}
+
+	return desireds, nil
+}
+
+func (store *FakeStore) DeleteDesiredState(desiredStates []models.DesiredAppState) error {
+	for _, state := range desiredStates {
+		_, present := store.desiredState[state.StoreKey()]
+		if !present {
+			return storeadapter.NewStoreError(storeadapter.StoreErrorKeyNotFound)
+		}
+		delete(store.desiredState, state.StoreKey())
+	}
+	return nil
+}
+
+func (store *FakeStore) SaveActualState(actualStates []models.InstanceHeartbeat) error {
+	for _, state := range actualStates {
+		store.actualState[state.StoreKey()] = state
+	}
+	return store.SaveActualStateError
+}
+
+func (store *FakeStore) GetActualState() ([]models.InstanceHeartbeat, error) {
+	if store.GetActualStateError != nil {
+		return []models.InstanceHeartbeat{}, store.GetActualStateError
+	}
+
+	actuals := make([]models.InstanceHeartbeat, len(store.actualState))
+
+	i := 0
+	for _, actual := range store.actualState {
+		actuals[i] = actual
+		i++
+	}
+
+	return actuals, nil
+}
+
+func (store *FakeStore) DeleteActualState(actualStates []models.InstanceHeartbeat) error {
+	for _, state := range actualStates {
+		_, present := store.actualState[state.StoreKey()]
+		if !present {
+			return storeadapter.NewStoreError(storeadapter.StoreErrorKeyNotFound)
+		}
+		delete(store.actualState, state.StoreKey())
+	}
+	return nil
+}
---
+++ testhelpers/fakestore/fake_store_test.go
@@ -0,0 +1,214 @@
+package fakestore_test
+
+import (
+	"errors"
+	. "github.com/cloudfoundry/hm9000/testhelpers/fakestore"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"github.com/cloudfoundry/hm9000/models"
+	"github.com/cloudfoundry/hm9000/storeadapter"
+	"github.com/cloudfoundry/hm9000/testhelpers/app"
+	. "github.com/cloudfoundry/hm9000/testhelpers/custommatchers"
+	"time"
+)
+
+var _ = Describe("FakeStore", func() {
+	var store *FakeStore
+	var app1 app.App
+	var app2 app.App
+
+	BeforeEach(func() {
+		store = NewFakeStore()
+		app1 = app.NewApp()
+		app2 = app.NewApp()
+	})
+
+	It("should start off empty", func() {
+		desired, err := store.GetDesiredState()
+		Ω(desired).Should(BeEmpty())
+		Ω(err).Should(BeNil())
+
+		actual, err := store.GetActualState()
+		Ω(actual).Should(BeEmpty())
+		Ω(err).Should(BeNil())
+
+		Ω(store.DesiredIsFresh).Should(BeFalse())
+		Ω(store.ActualIsFresh).Should(BeFalse())
+	})
+
+	Describe("bumping freshness", func() {
+		It("should support bumping freshness by saving a bool and the passed in timestamp", func() {
+			err := store.BumpDesiredFreshness(time.Unix(17, 0))
+			Ω(store.DesiredIsFresh).Should(BeTrue())
+			Ω(store.DesiredFreshnessTimestamp.Equal(time.Unix(17, 0))).Should(BeTrue())
+			Ω(err).ShouldNot(HaveOccured())
+
+			err = store.BumpActualFreshness(time.Unix(12, 0))
+			Ω(store.ActualIsFresh).Should(BeTrue())
+			Ω(store.ActualFreshnessTimestamp.Equal(time.Unix(12, 0))).Should(BeTrue())
+			Ω(err).ShouldNot(HaveOccured())
+
+			store.Reset()
+			Ω(store.DesiredIsFresh).Should(BeFalse())
+			Ω(store.DesiredFreshnessTimestamp).Should(BeZero())
+			Ω(store.ActualIsFresh).Should(BeFalse())
+			Ω(store.ActualFreshnessTimestamp).Should(BeZero())
+		})
+
+		It("should support returning errors", func() {
+			errIn := errors.New("foo")
+			store.BumpDesiredFreshnessError = errIn
+			err := store.BumpDesiredFreshness(time.Unix(17, 0))
+			Ω(err).Should(Equal(errIn))
+
+			store.BumpActualFreshnessError = errIn
+			err = store.BumpActualFreshness(time.Unix(17, 0))
+			Ω(err).Should(Equal(errIn))
+
+			store.Reset()
+
+			err = store.BumpDesiredFreshness(time.Unix(12, 0))
+			Ω(err).ShouldNot(HaveOccured())
+
+			err = store.BumpActualFreshness(time.Unix(12, 0))
+			Ω(err).ShouldNot(HaveOccured())
+		})
+	})
+
+	Describe("Setting, getting, and deleting desired state", func() {
+		It("should set, get, and delete the desired state", func() {
+			desired1 := app1.DesiredState(0)
+			desired2 := app2.DesiredState(0)
+
+			err := store.SaveDesiredState([]models.DesiredAppState{desired1, desired1, desired2})
+			Ω(err).ShouldNot(HaveOccured())
+
+			desired, err := store.GetDesiredState()
+			Ω(err).ShouldNot(HaveOccured())
+			Ω(desired).Should(HaveLen(2))
+			Ω(desired).Should(ContainElement(EqualDesiredState(desired1)))
+			Ω(desired).Should(ContainElement(EqualDesiredState(desired2)))
+
+			desired2.NumberOfInstances = 17
+			desired3 := app.NewApp().DesiredState(2)
+
+			err = store.SaveDesiredState([]models.DesiredAppState{desired2, desired3})
+			Ω(err).ShouldNot(HaveOccured())
+
+			desired, err = store.GetDesiredState()
+			Ω(err).ShouldNot(HaveOccured())
+			Ω(desired).Should(HaveLen(3))
+			Ω(desired).Should(ContainElement(EqualDesiredState(desired1)))
+			Ω(desired).Should(ContainElement(EqualDesiredState(desired2)))
+			Ω(desired).Should(ContainElement(EqualDesiredState(desired3)))
+
+			err = store.DeleteDesiredState([]models.DesiredAppState{desired2, desired3})
+			Ω(err).ShouldNot(HaveOccured())
+			desired, err = store.GetDesiredState()
+			Ω(err).ShouldNot(HaveOccured())
+			Ω(desired).Should(HaveLen(1))
+			Ω(desired).Should(ContainElement(EqualDesiredState(desired1)))
+
+			err = store.DeleteDesiredState([]models.DesiredAppState{desired2})
+			Ω(err).Should(HaveOccured())
+			Ω(storeadapter.IsKeyNotFoundError(err)).Should(BeTrue())
+
+			store.Reset()
+
+			desired, err = store.GetDesiredState()
+			Ω(desired).Should(BeEmpty())
+			Ω(err).Should(BeNil())
+		})
+
+		It("should support returning errors", func() {
+			desired1 := app1.DesiredState(0)
+			store.SaveDesiredState([]models.DesiredAppState{desired1})
+
+			errIn := errors.New("foo")
+
+			store.SaveDesiredStateError = errIn
+			err := store.SaveDesiredState([]models.DesiredAppState{desired1})
+			Ω(err).Should(Equal(errIn))
+
+			store.GetDesiredStateError = errIn
+			desired, err := store.GetDesiredState()
+			Ω(desired).Should(BeEmpty())
+			Ω(err).Should(Equal(errIn))
+
+			store.Reset()
+			err = store.SaveDesiredState([]models.DesiredAppState{desired1})
+			Ω(err).ShouldNot(HaveOccured())
+			_, err = store.GetDesiredState()
+			Ω(err).ShouldNot(HaveOccured())
+		})
+	})
+
+	Describe("Setting, getting, and deleting actual state", func() {
+		It("should set, get, and delete the actual state", func() {
+			heartbeat1 := app1.GetInstance(0).Heartbeat(12)
+			heartbeat2 := app2.GetInstance(0).Heartbeat(10)
+
+			err := store.SaveActualState([]models.InstanceHeartbeat{heartbeat1, heartbeat1, heartbeat2})
+			Ω(err).ShouldNot(HaveOccured())
+
+			actual, err := store.GetActualState()
+			Ω(err).ShouldNot(HaveOccured())
+			Ω(actual).Should(HaveLen(2))
+			Ω(actual).Should(ContainElement(heartbeat1))
+			Ω(actual).Should(ContainElement(heartbeat2))
+
+			heartbeat2.State = models.InstanceStateCrashed
+			heartbeat3 := app1.GetInstance(1).Heartbeat(12)
+
+			err = store.SaveActualState([]models.InstanceHeartbeat{heartbeat2, heartbeat3})
+			Ω(err).ShouldNot(HaveOccured())
+
+			actual, err = store.GetActualState()
+			Ω(err).ShouldNot(HaveOccured())
+			Ω(actual).Should(HaveLen(3))
+			Ω(actual).Should(ContainElement(heartbeat1))
+			Ω(actual).Should(ContainElement(heartbeat2))
+			Ω(actual).Should(ContainElement(heartbeat3))
+
+			err = store.DeleteActualState([]models.InstanceHeartbeat{heartbeat2, heartbeat3})
+			Ω(err).ShouldNot(HaveOccured())
+			actual, err = store.GetActualState()
+			Ω(err).ShouldNot(HaveOccured())
+			Ω(actual).Should(HaveLen(1))
+			Ω(actual).Should(ContainElement(heartbeat1))
+
+			err = store.DeleteActualState([]models.InstanceHeartbeat{heartbeat2})
+			Ω(err).Should(HaveOccured())
+			Ω(storeadapter.IsKeyNotFoundError(err)).Should(BeTrue())
+
+			store.Reset()
+
+			actual, err = store.GetActualState()
+			Ω(actual).Should(BeEmpty())
+			Ω(err).Should(BeNil())
+		})
+
+		It("should support returning errors", func() {
+			heartbeat1 := app1.GetInstance(0).Heartbeat(12)
+			store.SaveActualState([]models.InstanceHeartbeat{heartbeat1})
+
+			errIn := errors.New("foo")
+
+			store.SaveActualStateError = errIn
+			err := store.SaveActualState([]models.InstanceHeartbeat{heartbeat1})
+			Ω(err).Should(Equal(errIn))
+
+			store.GetActualStateError = errIn
+			desired, err := store.GetActualState()
+			Ω(desired).Should(BeEmpty())
+			Ω(err).Should(Equal(errIn))
+
+			store.Reset()
+			err = store.SaveActualState([]models.InstanceHeartbeat{heartbeat1})
+			Ω(err).ShouldNot(HaveOccured())
+			_, err = store.GetActualState()
+			Ω(err).ShouldNot(HaveOccured())
+		})
+	})
+})
