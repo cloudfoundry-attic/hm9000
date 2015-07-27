07579f7f9fe29c1cc08fbe2616a063b44bc6fa72
Joseph Palermo <jpalermo@pivotal.io>
2015-07-27 11:58:00 -0700

    Add test for verifying etcd server used under test is the correct version
    [#98993568]

--- store/store_suite_test.go
+++ store/store_suite_test.go
@@ -1,18 +1,22 @@
 package store_test
 
 import (
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
+	"testing"
+
+	"github.com/cloudfoundry/storeadapter/storerunner/etcdstorerunner"
 	. "github.com/onsi/ginkgo"
 	"github.com/onsi/ginkgo/config"
 	. "github.com/onsi/gomega"
-
-	"github.com/cloudfoundry/storeadapter/storerunner/etcdstorerunner"
-	"os"
-	"os/signal"
-
-	"testing"
 )
 
-var etcdRunner *etcdstorerunner.ETCDClusterRunner
+var (
+	etcdRunner  *etcdstorerunner.ETCDClusterRunner
+	etcdVersion = "2.1.0"
+)
 
 func TestStore(t *testing.T) {
 	registerSignalHandler()
@@ -21,11 +25,23 @@ func TestStore(t *testing.T) {
 	etcdRunner = etcdstorerunner.NewETCDClusterRunner(5001+config.GinkgoConfig.ParallelNode, 1)
 
 	etcdRunner.Start()
-
 	RunSpecs(t, "Store Suite")
-
 	etcdRunner.Stop()
 }
+
+var _ = BeforeSuite(func() {
+	Expect(len(etcdRunner.NodeURLS())).Should(BeNumerically(">=", 1))
+
+	etcdVersionUrl := etcdRunner.NodeURLS()[0] + "/version"
+	resp, err := http.Get(etcdVersionUrl)
+	Expect(err).ToNot(HaveOccurred())
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	Expect(err).ToNot(HaveOccurred())
+
+	Expect(string(body)).To(ContainSubstring(etcdVersion))
+})
 
 var _ = BeforeEach(func() {
 	etcdRunner.Reset()
